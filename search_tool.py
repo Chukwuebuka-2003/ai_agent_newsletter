f4a41fa2b8613b54d2c81b16edf3e24d104b5ee4
Chukwuebuka (Gaus Octavio ) <56232734+Chukwuebuka-2003@users.noreply.github.com>
2025-01-11 18:20:11 +0100

    Create search_tool.py

---
+++ search_tool.py
@@ -0,0 +1,26 @@
+from crewai.tools import BaseTool
+from exa_py import Exa
+from datetime import datetime, timedelta
+import os
+
+class SearchAndContents(BaseTool):
+    name: str = "Search and Contents Tool"
+    description: str = (
+        "Searches the web based on a search query for the latest results. Results are only from the last week. Uses the Exa API. This also returns the contents of the search results."
+    )
+
+    def _run(self, search_query: str) -> str:
+
+        exa = Exa(api_key=os.getenv("EXA_API_KEY"))
+
+        one_week_ago = datetime.now() - timedelta(days=7)
+        date_cutoff = one_week_ago.strftime("%Y-%m-%d")
+
+        search_results = exa.search_and_contents(
+            query=search_query,
+            use_autoprompt=True,
+            start_published_date=date_cutoff,
+            text={"include_html_tags": False, "max_characters": 8000},
+        )
+
+        return search_results
